28527f46e4a3fe1ac6042971b21f062dcc62ff76
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-10-15 13:47:17 +0200

    Adding a new node to the LinkedList

--- src/com/alaskalany/datastructures/LinkedList.java
+++ src/com/alaskalany/datastructures/LinkedList.java
@@ -5,4 +5,28 @@ package com.alaskalany.datastructures;
  */
 public class LinkedList {
 
+    Node first;
+
+    public LinkedList() {
+        first = null;
+    }
+
+    void add(int i) {
+        Node newNode = new Node(i, null);
+        if (first == null) {
+            first = newNode;
+        } else {
+            // 1. step find the last node
+            Node currentNode = first;
+            while (currentNode.next != null) {
+                // Current node is not the last node
+                currentNode = currentNode.next;
+            }
+            // current.next == null
+            // That means current node is the last node in this LinkedList
+            // 2. step insert new node
+            // Add the new node as the last node
+            currentNode.next = newNode;
+        }
+    }
 }
