7aa364567f1fb47bdbeaeca7dc6f98fa36dceefa
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-10-15 13:38:38 +0200

    Javadoc for the Node and LinkedList classes

--- src/com/alaskalany/datastructures/LinkedList.java
+++ src/com/alaskalany/datastructures/LinkedList.java
@@ -1,5 +1,8 @@
 package com.alaskalany.datastructures;
 
+/**
+ * Linked list data structure that can contain linked storage {@link Node}s
+ */
 public class LinkedList {
 
 }
--- src/com/alaskalany/datastructures/Node.java
+++ src/com/alaskalany/datastructures/Node.java
@@ -1,10 +1,25 @@
 package com.alaskalany.datastructures;
 
+/**
+ * A {@link LinkedList} node that can store an {@link int} element and be linked to a next {@link Node} in a {@link LinkedList}
+ */
 public class Node {
 
+    /**
+     * Storage element of the {@link Node}
+     */
     int elem;
+    /**
+     * Next {@link Node} in a {@link LinkedList}
+     */
     Node next;
 
+    /**
+     * Create a Node that can store an {@link int} element and be linked to a next {@link Node} in a {@link LinkedList}
+     *
+     * @param i Element to be stored in the {@link Node}
+     * @param n Next {@link Node} in a {@link LinkedList}
+     */
     Node(int i, Node n) {
         elem = i;
         next = n;
